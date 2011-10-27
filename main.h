234b9b4aa2acd6d95f27c514cfdfb58af76fca74
James Spencer <james.eats.world@gmail.com>
2011-10-27 14:13:52 +0100

    added project again and main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Oct 13, 2011
+ *      Author: Panda
+ */
+//#ifndef MAIN_H_
+//#define MAIN_H_
+
+/*class main {
+public:
+	main();
+	virtual ~main();
+};
+
+#endif /* MAIN_H_ */
+*/
